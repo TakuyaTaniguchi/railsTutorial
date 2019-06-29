32ae824c096d083a96d1e4312afd7c72f8d7bf03
ttaniguchi <ttaniguchi@oikaze.jp>
2019-06-29 17:54:52 +0900

    Update microposts

--- db/migrate/20190629085255_create_microposts.rb
+++ db/migrate/20190629085255_create_microposts.rb
@@ -2,7 +2,7 @@ class CreateMicroposts < ActiveRecord::Migration[5.2]
   def change
     create_table :microposts do |t|
       t.text :content
-      t.intege :user_id
+      t.integer :user_id
 
       t.timestamps
     end
