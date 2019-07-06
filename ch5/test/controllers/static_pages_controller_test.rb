f8a80478b0b0697cb02827c02ab859131bc4d2a2
ttaniguchi <ttaniguchi@oikaze.jp>
2019-07-06 13:49:37 +0900

    Update testを追加

--- ch5/test/controllers/static_pages_controller_test.rb
+++ ch5/test/controllers/static_pages_controller_test.rb
@@ -22,4 +22,9 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_select "title", "About | #{@base_title}"
   end
+  test "shouls get contact" do
+    get static_pages_contact_url
+    assert_response :success
+    assert_select "title","Contact | Ruby on Rails Tutorial Sample App"
+  end
 end
