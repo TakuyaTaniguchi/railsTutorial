00c7a0eeead554960028851bc446cd3c83aa353d
ttaniguchi <ttaniguchi@oikaze.jp>
2019-07-26 10:37:20 +0900

    Update usertest

--- ch6/test/models/user_test.rb
+++ ch6/test/models/user_test.rb
@@ -2,7 +2,8 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   def setup
-    @user = User.new(name: "Example User", email: "user@example.com");
+    @user = User.new(name: "Example User", email: "user@example.com",
+      password: "foobar", password_confirmation: "foobar")
   end
   
   test "should be valid" do
@@ -43,12 +44,12 @@ class UserTest < ActiveSupport::TestCase
     duplicate_user.email = @user.email.upcase
     @user.save
     assert_not  duplicate_user.valid?
-  end
+  endd
 
   test "email address should be unique" do
     duplicate_user = @user.dup
     duplicate_user.email = @user.email.upcase
     @user.save
     assert_not duplicate_user.valid?
+  end
 end
-
