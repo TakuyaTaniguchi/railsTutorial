d7d2b149285fe97656af1cd9d0ba3f98064c2ce1
ttaniguchi <ttaniguchi@oikaze.jp>
2019-07-26 11:13:33 +0900

    Update userフォームの作成 6.4.1

--- ch6/test/models/user_test.rb
+++ ch6/test/models/user_test.rb
@@ -44,7 +44,7 @@ class UserTest < ActiveSupport::TestCase
     duplicate_user.email = @user.email.upcase
     @user.save
     assert_not  duplicate_user.valid?
-  endd
+  end
 
   test "email address should be unique" do
     duplicate_user = @user.dup
@@ -52,4 +52,15 @@ class UserTest < ActiveSupport::TestCase
     @user.save
     assert_not duplicate_user.valid?
   end
+
+  test "password should be present (nonblank)" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
+
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
+  end
+
 end
